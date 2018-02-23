678c62ef38ab8d64722f7e5a3d636f5e0c5edf97
robert <rob.drynkin@gmail.com>
2018-02-23 14:29:05 +0300

    Network connection added

---
+++ network/connect.sh
@@ -0,0 +1,2 @@
+geth --datadir ./chain-data init genesis.json
+geth --datadir ./chain-data --port 30259 --networkid 5128794 --bootnodes enode://f021cbd384eec489b081d451189238b8d5cd0851459c0e51db773de455eed13c6e259d0f2c7b86c3a665e17eecdbedcba07544ee75c1237dcccb8938fcc866be@5.23.52.206:30259
