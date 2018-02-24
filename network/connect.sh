b9dec878d2a35807e095bdc4462db14a03bfc5ce
robert <rob.drynkin@gmail.com>
2018-02-24 14:17:49 +0300

    Init script divided into 2 scripts

--- network/connect.sh
+++ network/connect.sh
@@ -1,2 +1,2 @@
-geth --datadir ./chain-data init CustomGenesis.json
+geth --datadir ./chain-data init GenesisBlock.json
 geth --datadir ./chain-data --port 30259 --networkid 5128794 --bootnodes enode://2fd46b0fce2cde2d0d2b77d6e5a0912e6caba6b461ce8089f34d1a5adbe83b10a3ff547cdf2363970d5c56788cc1c58c17234788adef127446a59e35f5c70371@5.23.52.206:30259
