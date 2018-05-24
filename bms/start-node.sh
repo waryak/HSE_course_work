6fcd46ba6b3104481278d2e077897d80bd7cc6da
robert <rob.drynkin@gmail.com>
2018-05-24 23:25:18 +0300

    Permissions fixed

--- bms/start-node.sh
+++ bms/start-node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 FILE_NAME="./data/queue_name.txt"
-./get-hostname.sh $FILE_NAME
-HOST_NAME=$(cat $FILE_NAME)
-celery -A bms worker -Q $HOST_NAME --time-limit 30 --concurrency=1 -l info
+bash get-hostname.sh ${FILE_NAME}
+HOST_NAME=$(cat ${FILE_NAME})
+celery -A bms worker -Q ${HOST_NAME} --time-limit 30 --concurrency=1 -l info
